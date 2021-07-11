1508dd9d51f173d5b604bb0ece0a346a53313bb6
Marcio Goveia <marciosg4@gmail.com>
2021-07-11 00:47:24 -0300

    challenge solved

--- src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
@@ -6,7 +6,13 @@ class ListAllUsersController {
   constructor(private listAllUsersUseCase: ListAllUsersUseCase) {}
 
   handle(request: Request, response: Response): Response {
-    // Complete aqui
+    const { user_id } = request.headers as { [key: string]: string };
+    try {
+      const all = this.listAllUsersUseCase.execute({ user_id });
+      return response.status(200).json(all);
+    } catch (error) {
+      return response.status(400).json({ error });
+    }
   }
 }
 
